aab36c4c04017bb531fe24c07b49cb570730afa2
silvaan <s.siilvan@yahoo.com>
2022-05-06 13:07:46 +0600

    Firebase init fixed issue

--- src/Firebase.init.js
+++ src/Firebase.init.js
@@ -14,15 +14,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_appId,
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCgHuP-Yy-T-yvHAxWVhNhuPauBYZwRjy0",
-//     authDomain: "product-inventory-f466c.firebaseapp.com",
-//     projectId: "product-inventory-f466c",
-//     storageBucket: "product-inventory-f466c.appspot.com",
-//     messagingSenderId: "537570905709",
-//     appId: "1:537570905709:web:47eba02b45a537db3cf964"
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
